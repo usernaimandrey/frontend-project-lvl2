5677b40556b6cf0f9c757436867cb90e1da63525
usernaimandrey <anshlyapnikov@yandex.ru>
2021-05-25 18:01:22 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,7 @@ import program from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0');
-program.parse();
+  .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
+program.parse(process.argv);
