7a5d13ca3b05b5abb7b8142bc81e70ed8b503fcc
usernaimandrey <anshlyapnikov@yandex.ru>
2021-06-16 15:44:55 +0300

    fix parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -2,11 +2,11 @@ import { extname } from 'path';
 import yaml from 'js-yaml';
 
 export default (data, fileName) => {
-  const format = extname(fileName);
-  if (format === '.json') {
+  const format = extname(fileName).slice(1);
+  if (format === 'json') {
     return JSON.parse(data);
   }
-  if (format === '.yml' || format === '.yaml') {
+  if (format === 'yml' || format === 'yaml') {
     return yaml.load(data);
   }
   return console.log('Error unknown file format!!!');
