f25f5ae8448a2fdfab73bdd873a0c661fbc4c7e0
usernaimandrey <anshlyapnikov@yandex.ru>
2021-06-17 11:57:09 +0300

    rm filetransform.js

--- src/fileTransform.js
+++
@@ -1,9 +0,0 @@
-import { readFile } from './workWhithFiles.js';
-import parsers from './parsers.js';
-
-const fileTransform = (fileName) => {
-  const data = readFile(fileName);
-  return parsers(data, fileName);
-};
-
-export default fileTransform;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import fileTransform from './fileTransform.js';
 import buildAst from './formater/buildAst.js';
 import formatSwitches from './formater/index.js';
+import { readFile } from './workWhithFiles.js';
+import parsers from './parsers.js';
 
+const fileTransform = (fileName) => {
+  const data = readFile(fileName);
+  return parsers(data, fileName);
+};
 const genDiff = (filePath1, filePath2, format = 'stylish') => {
   const objBefore = fileTransform(filePath1);
   const objAfter = fileTransform(filePath2);
